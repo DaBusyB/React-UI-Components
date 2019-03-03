6b765a19673a1e522d776192a42a919f837202ea
Brandi Apetsi <Brandi@Brandis-iMac.local>
2019-03-03 18:06:00 -0500

    mvp

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,7 +5,10 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer'>
-
+            {/* <img src='' alt='comment bubble' />
+            <img src='' alt='refresh icon' />
+            <img src='' alt='like icon' />
+            <img src='' alt='pm icon' /> */}
         </div>
     );
 }
