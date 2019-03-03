2584162942b54bc86b0b7971b39d5056a285576e
Brandi Apetsi <Brandi@Brandis-iMac.local>
2019-03-03 00:38:48 -0500

    finished card body

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -5,9 +5,11 @@ import './Card.css';
 const CardContent = () => {
     return(
         <div className='cardContent'>
-            <h2>Get started with React</h2>
-            <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
-            <p>reactjs.org</p>
+            <div >
+                <h2>Get started with React</h2>
+                <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+                <p>reactjs.org</p>
+            </div>
         </div>
     );
 }
