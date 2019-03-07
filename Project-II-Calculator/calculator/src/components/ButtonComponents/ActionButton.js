b446b8738cc608da52311a3cb7909ba42143c1d3
Brandi Apetsi <Brandi@Brandis-iMac.local>
2019-03-06 20:04:23 -0500

    calc work

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,13 @@
 import React from 'react';
+
 import './Button.css';
+
+const ActionButton = () => {
+    return(
+        <div className='clearZeroBtn'>
+            <p className='clearZeroBtnTxt'> clear </p>
+        </div>
+    );
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
+
 import './Display.css';
+
+const CalculatorDisplay = () => {
+    return(
+        <div className='displayContainer'>
+            <p className='displayContainerText'> 0 </p>
+        </div>
+    );
+}
+
+export default CalculatorDisplay;
